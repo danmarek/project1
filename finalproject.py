12f1d6c7fe5c9975d5625f1fc1800af5a9236385
Dan Marek <dan.marek@linesimple.com>
2017-11-21 22:03:33 -0600

    added port

--- finalproject.py
+++ finalproject.py
@@ -130,4 +130,4 @@ def deleteMenuItem(restaurant_id,menu_id):
 if __name__ == '__main__':
     app.secret_key = 'super_key_encode'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 80)
